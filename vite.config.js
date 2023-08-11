81ccde49a38906ccce313d9aa2ad75b5a9e0a931
Rizkyyoga20 <rizkyyoga281@gmail.com>
2023-08-11 21:12:30 +0700

    konfigurasi website lagi

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'rizkyyoga20.github.io/kopi-torra.github.io',
+  base: 'https://rizkyyoga20.github.io/kopi-torra.github.io',
   plugins: [vue()],
   //preview: {port: 8000,}
 })
