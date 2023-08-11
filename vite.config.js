6e913a6691ddd9798256d7aec83e14e938e68e2e
Rizkyyoga20 <rizkyyoga281@gmail.com>
2023-08-11 09:15:08 +0700

    update terbaru

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+  	//port: 8000,
+  	port:  "https://rizkyyoga20.github.io/kopi-torra.github.io",
+  }
 })
 
 
