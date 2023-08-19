2dc5736fc7ee40fc27f3f3af2efa97f883d99421
Rizkyyoga20 <rizkyyoga281@gmail.com>
2023-08-19 11:12:41 +0700

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/kopi-torra.github.io.git/',
   plugins: [vue()],
   //preview: {port: 8000,}
  })
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/kopi-torra.github.io.git/" : "/",
+};
